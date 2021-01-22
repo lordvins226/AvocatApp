c0cd2f503305f28d45994763e5d05ca1e2f4fbc9
lordvins226 <kevineilboudo54@gmail.com>
2021-01-22 18:34:58 +0000

    Initial Commit

---
+++ AcocatApp/AcocatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AcocatAppApp.swift
+//  AcocatApp
+//
+//  Created by Kevin on 22/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AcocatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
